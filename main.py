42528d9b5f20a36943220eb83cebbc0670a16ca2
hayden <99268163+haydenhem@users.noreply.github.com>
2022-10-05 18:19:57 -0400

    simplified / recoded

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 import shutil
-path = "/"
-def free(path):
-    stat = str(shutil.disk_usage(path))
-    yes = stat.split("free=",1)[1]
-    return yes.replace(")","")
-print("Bytes Free:",free(path))
-FILENAME = "getboomed.txt"
-SIZE = int(free(path))
-okay = input("Are you ready to boom? This will fill whatever drive you are running this script on. Type anything to start!")
-while True:
-    if SIZE > 100:
-        with open(FILENAME, "wb") as file:
-            file.write(b"\0" * SIZE)
-            print("Bytes Free:",free(path))
-            print("DONE BOOMBOOM")
-    print("If you are seeing this than the PC has ran out of space. Bytes left: ", SIZE)
 
-    
+tb = 1099511627776
+gb = 1073741824
+mb = 1048576
+kb = 1024
+size = 0
+yes = input("(KB/MB/GB/TB/FILL) ")
+if yes.__contains__("k"):
+    manykbs = input("How many KBS? ")
+    size = int(manykbs) * kb
+if yes.__contains__("m"):
+    manymbs = input("How many MBS? ")
+    size = int(manymbs) * mb
+if yes.__contains__("g"):
+    manygbs = input("How many GBS? ")
+    size = int(manygbs) * gb
+if yes.__contains__("t"):
+    manytbs = input("How many TBS? ")
+    size = int(manytbs) * tb
+if yes.__contains__("fill"):
+    manytbs = input("Are you sure you want to fill your drive? This will be generating "+str(shutil.disk_usage('/').free / gb) + " gigabytes. Type anything than press enter to continue.")
+    size = shutil.disk_usage()
+with open('boomed', 'wb') as f:
+    f.seek(size) # One GB
+    f.write(b'0')
+print('Done')
